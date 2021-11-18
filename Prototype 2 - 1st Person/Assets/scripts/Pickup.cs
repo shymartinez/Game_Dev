6419e8604f5bfdd8b8b8fc96f01468eaf15395ad
shymartinez <shayla.m216@stu.nebo.edu>
2021-11-18 09:21:16 -0700

    animation and particle system

--- Prototype 2 - 1st Person/Assets/scripts/Pickup.cs
+++ Prototype 2 - 1st Person/Assets/scripts/Pickup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public enum PickupType
 {
     Health,
@@ -12,15 +13,19 @@ public class Pickup : MonoBehaviour
     public PickupType type;
     public int value;
 
+    [Header("Bobbing Anim")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //set the start position 
+        startPos = transform.position;
     }
     void OnTriggerEnter(Collider other)
     {
@@ -39,6 +44,18 @@ public class Pickup : MonoBehaviour
             }
             Destroy(gameObject);
         }
+    }
+    void Update()
+    {
+        //rotating 
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        //Bob up and down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight / 2, 0) : new Vector3(0, -bobHeight / 2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if(transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
     }
 }
 
